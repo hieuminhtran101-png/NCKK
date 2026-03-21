0eafa22deb36ef3b1ae1034d345b486c8112f9d1
Tran Minh Hieu <hieuminhtran101@gmail.com>
2026-03-21 21:09:03 +0700

    Update main.py before merging to main

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://nckk-u8qo.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
